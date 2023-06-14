6f70eb890396d0a79f1dcad9c09b51f2f11c8e63
Haroune Mohammedi <haroune.mohammedi@quadratic-labs.com>
2023-06-14 19:00:04 +0200

    Minor refactoring

--- example.ts
+++ example.ts
@@ -15,12 +15,12 @@ type TypedCall<TAbi extends Abi> = {
   ): Promise<FunctionRet<TAbi, TFunctionName>>;
 };
 
-type TypedContractNew<TAbi extends Abi> = TypedCall<TAbi> &
+type TypedContract<TAbi extends Abi> = TypedCall<TAbi> &
   ContractFunctions<TAbi>;
 
 function createTypedContract<TAbi extends Abi>(
   contract: Contract
-): TypedContractNew<TAbi> {
+): TypedContract<TAbi> {
   const typedContract = {
     async call<TFunctionName extends ExtractAbiFunctionNames<TAbi>>(
       method: TFunctionName,
@@ -28,21 +28,21 @@ function createTypedContract<TAbi extends Abi>(
     ): Promise<FunctionRet<TAbi, TFunctionName>> {
       const args_array: any[] =
         args === undefined ? [] : Array.isArray(args) ? args : [args];
-
       return (await contract.call(
         method,
         args_array,
         undefined
       )) as FunctionRet<TAbi, TFunctionName>;
     },
-  } as TypedContractNew<TAbi>;
+  } as TypedContract<TAbi>;
 
   contract.abi.forEach((abiElement) => {
-    if (abiElement.type !== "function") return;
-    Object.defineProperty(typedContract, abiElement.name, {
-      writable: true,
-      value: buildDefault(contract, abiElement),
-    });
+    if (abiElement.type === "function") {
+      Object.defineProperty(typedContract, abiElement.name, {
+        writable: true,
+        value: buildDefault(contract, abiElement),
+      });
+    }
   });
 
   return typedContract;
@@ -55,28 +55,29 @@ async function main() {
 
   const testAddress =
     "0x01c6b2a993b335cabc12dd5fa39875f456f07f23b10dcbdf6529667a322bbda4";
-
   const { abi: testAbi } = await provider.getClassAt(testAddress);
-  if (testAbi === undefined) {
-    throw new Error("no abi.");
-  }
-  const myTestContract = new Contract(testAbi, testAddress, provider);
 
+  if (testAbi === undefined) {
+    throw new Error("No ABI.");
+  }
+
+  const myTestContract = new Contract(testAbi, testAddress, provider);
   const myTypedContract = createTypedContract<typeof abi>(myTestContract);
 
-  const tbal1 = await myTypedContract.call("get_balance", []);
-  console.log("Typed Initial balance =", tbal1);
-
-  const tbal2 = await myTypedContract.get_balance();
-  console.log("Typed Initial balance meta-class =", tbal2);
-
+  console.log(
+    "Typed Initial balance =",
+    await myTypedContract.call("get_balance", [])
+  );
+  console.log(
+    "Typed Initial balance meta-class =",
+    await myTypedContract.get_balance()
+  );
   console.log("Typed get_status =", await myTypedContract.get_status());
-
-  const bal1 = await myTestContract.call("get_balance");
-  console.log("Initial balance =", bal1);
-
-  const bal2 = await myTestContract.get_balance();
-  console.log("Initial balance meta-class =", bal1);
+  console.log("Initial balance =", await myTestContract.call("get_balance"));
+  console.log(
+    "Initial balance meta-class =",
+    await myTestContract.get_balance()
+  );
 }
 
 main();
